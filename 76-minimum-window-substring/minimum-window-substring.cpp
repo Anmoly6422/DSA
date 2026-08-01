8904d8efaaa4f046a907e4c8daa494b6dabc34e3
Anmol Yadav <148643693+Anmoly6422@users.noreply.github.com>
2026-08-01 23:22:23 +0530

    Time: 9 ms (52.08%) | Memory: 11.6 MB (43.43%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+
+        unordered_map<char, int> mp;
+
+        // Store frequency of characters in t
+        for (char c : t)
+            mp[c]++;
+
+        int left = 0;
+        int count = 0;
+
+        int minLen = INT_MAX;
+        int start = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            // If current character is needed
+            if (mp[s[right]] > 0)
+                count++;
+
+            // Decrease its frequency
+            mp[s[right]]--;
+
+            // Window is valid
+            while (count == t.size()) {
+
+                // Update answer
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                // Remove left character
+                mp[s[left]]++;
+
+                // If it becomes needed again,
+                // window is no longer valid
+                if (mp[s[left]] > 0)
+                    count--;
+
+                left++;
+            }
+        }
+
+        if (minLen == INT_MAX)
+            return "";
+
+        return s.substr(start, minLen);
+    }
+};
